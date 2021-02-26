581155466f606deaa826089da8edb819b0f0904e
Priscilla de Roode <priscilladeroode@gmail.com>
2021-02-26 12:46:29 -0300

    Modal de início de novo level

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,7 @@ import { ChallengesProvider } from '../contexts/ChallengesContext'
 import { useState } from 'react'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ChallengesProvider>
-      <Component {...pageProps} />
-    </ChallengesProvider>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
